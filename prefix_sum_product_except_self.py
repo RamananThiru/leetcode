132857b764a52c2204405e35197fdf132a14006a
Ramanan <52417045+RamananThiru@users.noreply.github.com>
2024-04-09 22:29:26 +0530

    Update prefix_sum_product_except_self.py

--- prefix_sum_product_except_self.py
+++ prefix_sum_product_except_self.py
@@ -48,5 +48,30 @@ class Solution:
 """
 
 Solution 2:
+
+Have two arrays , one array that calculates the prefix from product from left to right 
+Another array , that calculates the prefix product from right to left
 """
+
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        left = [1] * n
+        right = [1] * n
+        
+        # Calculate left prefix product
+        for i in range(1, n):
+            left[i] = left[i - 1] * nums[i - 1]
+
+        # Calculate right suffix product
+        for i in range(n - 2, -1, -1):
+            right[i] = right[i + 1] * nums[i + 1]
+        print(left, right)    
+
+        # Calculate result array
+        result = [left[i] * right[i] for i in range(n)]
+       
+        return result
 
